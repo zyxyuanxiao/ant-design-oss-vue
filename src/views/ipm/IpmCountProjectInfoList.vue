eddce71e3ba4913a8144e2c9d727d511a11d4753
RunningOu <597395581@qq.com>
2020-09-23 18:53:20 +0800

    新增了IP地址统计界面

---
+++ src/views/ipm/IpmCountProjectInfoList.vue
@@ -0,0 +1,201 @@
+<template>
+  <a-card :bordered="false">
+    <!-- 查询区域 -->
+    <div class="table-page-search-wrapper">
+      <a-form layout="inline" @keyup.enter.native="searchQuery">
+        <a-row :gutter="24">
+          <a-col :xl="6" :lg="7" :md="8" :sm="24">
+            <a-form-item label="项目信息">
+              <j-search-select-tag placeholder="请选择项目信息" v-model="queryParam.project" dict=",,project"/>
+            </a-form-item>
+          </a-col>
+          <a-col :xl="10" :lg="11" :md="12" :sm="24">
+            <a-form-item label="在线率">
+              <a-input placeholder="请输入最小值" class="query-group-cust" v-model="queryParam.onlineRate_begin"></a-input>
+              <span class="query-group-split-cust"></span>
+              <a-input placeholder="请输入最大值" class="query-group-cust" v-model="queryParam.onlineRate_end"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xl="6" :lg="7" :md="8" :sm="24">
+            <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
+              <a-button type="primary" @click="searchQuery" icon="search">查询</a-button>
+              <a-button type="primary" @click="searchReset" icon="reload" style="margin-left: 8px">重置</a-button>
+              <a @click="handleToggleSearch" style="margin-left: 8px">
+                {{ toggleSearchStatus ? '收起' : '展开' }}
+                <a-icon :type="toggleSearchStatus ? 'up' : 'down'"/>
+              </a>
+            </span>
+          </a-col>
+        </a-row>
+      </a-form>
+    </div>
+    <!-- 查询区域-END -->
+
+    <!-- 操作按钮区域 -->
+    <div class="table-operator">
+<!--      <a-button @click="handleAdd" type="primary" icon="plus">新增</a-button>-->
+      <a-button type="primary" icon="download" @click="handleExportXls('oss_ipm_count_project_info')">导出</a-button>
+<!--      <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">-->
+<!--        <a-button type="primary" icon="import">导入</a-button>-->
+<!--      </a-upload>-->
+<!--      <a-dropdown v-if="selectedRowKeys.length > 0">-->
+<!--        <a-menu slot="overlay">-->
+<!--          <a-menu-item key="1" @click="batchDel"><a-icon type="delete"/>删除</a-menu-item>-->
+<!--        </a-menu>-->
+<!--        <a-button style="margin-left: 8px"> 批量操作 <a-icon type="down" /></a-button>-->
+<!--      </a-dropdown>-->
+    </div>
+
+    <!-- table区域-begin -->
+    <div>
+      <div class="ant-alert ant-alert-info" style="margin-bottom: 16px;">
+        <i class="anticon anticon-info-circle ant-alert-icon"></i> 已选择 <a style="font-weight: 600">{{ selectedRowKeys.length }}</a>项
+        <a style="margin-left: 24px" @click="onClearSelected">清空</a>
+      </div>
+
+      <a-table
+        ref="table"
+        size="middle"
+        bordered
+        rowKey="id"
+        :columns="columns"
+        :dataSource="dataSource"
+        :pagination="ipagination"
+        :loading="loading"
+        :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
+        class="j-table-force-nowrap"
+        @change="handleTableChange">
+
+        <template slot="htmlSlot" slot-scope="text">
+          <div v-html="text"></div>
+        </template>
+        <template slot="imgSlot" slot-scope="text">
+          <span v-if="!text" style="font-size: 12px;font-style: italic;">无图片</span>
+          <img v-else :src="getImgView(text)" height="25px" alt="" style="max-width:80px;font-size: 12px;font-style: italic;"/>
+        </template>
+        <template slot="fileSlot" slot-scope="text">
+          <span v-if="!text" style="font-size: 12px;font-style: italic;">无文件</span>
+          <a-button
+            v-else
+            :ghost="true"
+            type="primary"
+            icon="download"
+            size="small"
+            @click="uploadFile(text)">
+            下载
+          </a-button>
+        </template>
+
+<!--        <span slot="action" slot-scope="text, record">-->
+<!--          <a @click="handleEdit(record)">编辑</a>-->
+
+<!--          <a-divider type="vertical" />-->
+<!--          <a-dropdown>-->
+<!--            <a class="ant-dropdown-link">更多 <a-icon type="down" /></a>-->
+<!--            <a-menu slot="overlay">-->
+<!--              <a-menu-item>-->
+<!--                <a-popconfirm title="确定删除吗?" @confirm="() => handleDelete(record.id)">-->
+<!--                  <a>删除</a>-->
+<!--                </a-popconfirm>-->
+<!--              </a-menu-item>-->
+<!--            </a-menu>-->
+<!--          </a-dropdown>-->
+<!--        </span>-->
+
+      </a-table>
+    </div>
+
+    <ipmCountProjectInfo-modal ref="modalForm" @ok="modalFormOk"></ipmCountProjectInfo-modal>
+  </a-card>
+</template>
+
+<script>
+
+  import '@/assets/less/TableExpand.less'
+  import { mixinDevice } from '@/utils/mixin'
+  import { JeecgListMixin } from '@/mixins/JeecgListMixin'
+  import IpmCountProjectInfoModal from './modules/IpmCountProjectInfoModal'
+  import {filterMultiDictText} from '@/components/dict/JDictSelectUtil'
+  import JSearchSelectTag from '@/components/dict/JSearchSelectTag'
+
+  export default {
+    name: "IpmCountProjectInfoList",
+    mixins:[JeecgListMixin, mixinDevice],
+    components: {
+      JSearchSelectTag,
+      IpmCountProjectInfoModal
+    },
+    data () {
+      return {
+        description: 'oss_ipm_count_project_info管理页面',
+        // 表头
+        columns: [
+          {
+            title: '#',
+            dataIndex: '',
+            key:'rowIndex',
+            width:60,
+            align:"center",
+            customRender:function (t,r,index) {
+              return parseInt(index)+1;
+            }
+          },
+          {
+            title:'项目信息',
+            align:"center",
+            dataIndex: 'project_dictText'
+          },
+          {
+            title:'在线数量',
+            align:"center",
+            dataIndex: 'onlineCount'
+          },
+          {
+            title:'离线数量',
+            align:"center",
+            dataIndex: 'unonlineCount'
+          },
+          {
+            title:'在线率（%）',
+            align:"center",
+            dataIndex: 'onlineRate'
+          },
+          {
+            title:'统计时间',
+            align:"center",
+            dataIndex: 'createTime'
+          }
+          // ,
+          // {
+          //   title: '操作',
+          //   dataIndex: 'action',
+          //   align:"center",
+          //   // fixed:"right",
+          //   width:147,
+          //   scopedSlots: { customRender: 'action' }
+          // }
+        ],
+        url: {
+          list: "/ipm/ipmCountProjectInfo/list",
+          delete: "/ipm/ipmCountProjectInfo/delete",
+          deleteBatch: "/ipm/ipmCountProjectInfo/deleteBatch",
+          exportXlsUrl: "/ipm/ipmCountProjectInfo/exportXls",
+          importExcelUrl: "ipm/ipmCountProjectInfo/importExcel",
+        },
+        dictOptions:{},
+      }
+    },
+    computed: {
+      importExcelUrl: function(){
+        return `${window._CONFIG['domianURL']}/${this.url.importExcelUrl}`;
+      },
+    },
+    methods: {
+      initDictConfig(){
+      }
+    }
+  }
+</script>
+<style scoped>
+  @import '~@assets/less/common.less';
+</style>
---
+++ src/views/ipm/modules/IpmCountProjectInfoModal__Style#Drawer.vue
@@ -0,0 +1,145 @@
+<template>
+  <a-drawer
+    :title="title"
+    :width="width"
+    placement="right"
+    :closable="false"
+    @close="close"
+    :visible="visible">
+  
+    <a-spin :spinning="confirmLoading">
+      <a-form :form="form">
+
+        <a-form-item label="项目信息" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-input v-decorator="['project']" placeholder="请输入项目信息"></a-input>
+        </a-form-item>
+        <a-form-item label="在线数量" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-input-number v-decorator="['onlineCount']" placeholder="请输入在线数量" style="width: 100%"/>
+        </a-form-item>
+        <a-form-item label="离线数量" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-input-number v-decorator="['unonlineCount']" placeholder="请输入离线数量" style="width: 100%"/>
+        </a-form-item>
+        <a-form-item label="在线率" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-input-number v-decorator="['onlineRate']" placeholder="请输入在线率" style="width: 100%"/>
+        </a-form-item>
+        <a-form-item label="统计时间" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <j-date placeholder="请选择统计时间" v-decorator="['createTime', validatorRules.createTime]" :trigger-change="true" :show-time="true" date-format="YYYY-MM-DD HH:mm:ss" style="width: 100%"/>
+        </a-form-item>
+        
+      </a-form>
+    </a-spin>
+    <a-button type="primary" @click="handleOk">确定</a-button>
+    <a-button type="primary" @click="handleCancel">取消</a-button>
+  </a-drawer>
+</template>
+
+<script>
+
+  import { httpAction } from '@/api/manage'
+  import pick from 'lodash.pick'
+  import { validateDuplicateValue } from '@/utils/util'
+  import JDate from '@/components/jeecg/JDate'  
+  
+  export default {
+    name: "IpmCountProjectInfoModal",
+    components: { 
+      JDate,
+    },
+    data () {
+      return {
+        form: this.$form.createForm(this),
+        title:"操作",
+        width:800,
+        visible: false,
+        model: {},
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 5 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 },
+        },
+        confirmLoading: false,
+        validatorRules: {
+          createTime: {
+            rules: [
+              { required: true, message: '请输入统计时间!'},
+            ]
+          },
+        },
+        url: {
+          add: "/ipm/ipmCountProjectInfo/add",
+          edit: "/ipm/ipmCountProjectInfo/edit",
+        }
+      }
+    },
+    created () {
+    },
+    methods: {
+      add () {
+        this.edit({});
+      },
+      edit (record) {
+        this.form.resetFields();
+        this.model = Object.assign({}, record);
+        this.visible = true;
+        this.$nextTick(() => {
+          this.form.setFieldsValue(pick(this.model,'project','onlineCount','unonlineCount','onlineRate','createTime'))
+        })
+      },
+      close () {
+        this.$emit('close');
+        this.visible = false;
+      },
+      handleOk () {
+        const that = this;
+        // 触发表单验证
+        this.form.validateFields((err, values) => {
+          if (!err) {
+            that.confirmLoading = true;
+            let httpurl = '';
+            let method = '';
+            if(!this.model.id){
+              httpurl+=this.url.add;
+              method = 'post';
+            }else{
+              httpurl+=this.url.edit;
+               method = 'put';
+            }
+            let formData = Object.assign(this.model, values);
+            console.log("表单提交数据",formData)
+            httpAction(httpurl,formData,method).then((res)=>{
+              if(res.success){
+                that.$message.success(res.message);
+                that.$emit('ok');
+              }else{
+                that.$message.warning(res.message);
+              }
+            }).finally(() => {
+              that.confirmLoading = false;
+              that.close();
+            })
+          }
+         
+        })
+      },
+      handleCancel () {
+        this.close()
+      },
+      popupCallback(row){
+        this.form.setFieldsValue(pick(row,'project','onlineCount','unonlineCount','onlineRate','createTime'))
+      }
+      
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+/** Button按钮间距 */
+  .ant-btn {
+    margin-left: 30px;
+    margin-bottom: 30px;
+    float: right;
+  }
+</style>
